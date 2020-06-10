b065d0bffd2c3d5d66284f4ceb19085c8d7088fb
jpromanonet <programming@jpromano.net>
2020-06-10 12:28:14 -0600

    Calling libraries

---
+++ covid_stats.py
@@ -0,0 +1,14 @@
+# To install before using this script
+
+## python -m pip install COVID19py
+## python -m pip install matplotlib
+
+# Now we import the libraries
+
+import COVID19py
+import matplotlib.pyplot as mpl 
+
+# Declaring and assigning variables
+
+covid19 = COVID19py.COVID19()
+data = covid19.getAll(timelines=True)
